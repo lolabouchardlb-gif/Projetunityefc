1d81411511a20dbfb35ec2e522a5b2d3768342b9
lolabouchardlb-gif <lolabouchard.lb@gmail.com>
2026-05-19 10:12:35 -0400

    lola partie
    mettre les vrai infos et le son click

---
+++ Assets/Scripts/ClickSoundManager.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickSoundManager : MonoBehaviour
+{
+    public static ClickSoundManager Instance;
+
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioClip _clickSound;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        AddSoundsToButtons();
+    }
+
+    private void OnEnable()
+    {
+        UnityEngine.SceneManagement.SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnDisable()
+    {
+        UnityEngine.SceneManagement.SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(UnityEngine.SceneManagement.Scene scene,
+                               UnityEngine.SceneManagement.LoadSceneMode mode)
+    {
+        AddSoundsToButtons();
+    }
+
+    private void AddSoundsToButtons()
+    {
+        Button[] allButtons = FindObjectsOfType<Button>(true);
+
+        foreach (Button btn in allButtons)
+        {
+            btn.onClick.RemoveListener(PlayClick);
+            btn.onClick.AddListener(PlayClick);
+        }
+    }
+
+    private void PlayClick()
+    {
+        _audioSource.PlayOneShot(_clickSound);
+    }
+}
